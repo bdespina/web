3913453d0bc4a39026e82b30d178d59e4c786ffc
Valilo <58746615+Valilo@users.noreply.github.com>
2021-09-11 21:13:13 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,18 @@
 const express = require('express');
-const session = require('express-session');
 const router = express.Router();
 const services = require('../services');
-const user = require ('../services');
-var sqlite3 = require('sqlite3').verbose();
-var file = "./database/db.sqlite";
-var db = new sqlite3.Database(file);
 
+const multer = require('multer');
+const upload = multer({ dest: './uploads' });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
 router.get('/register', function(req, res, next) {
   res.render('register', { title: 'Register' });
 });
-
 
 router.post('/register', function(req, res, next) {
   services.registerUser(req.body).then(done => {
@@ -27,66 +22,105 @@ router.post('/register', function(req, res, next) {
   });
 });
 
-
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
-  
 router.post('/login', function(req, res, next) {
-  var username = req.body.username;
-  var password = req.body.password;
-  db.all("SELECT role FROM users where username = ? and password=?",[username,password] ,function(err, rows) {
-    rows.forEach(function (row) {
-        session.role = row.role;
-    })
-  });	
-  db.close();
   services.loginUser(req.body).then(done => {
     req.session.isAuthenticated = true;
-    console.log (session.role);
-    if ( !session.role) {
-      res.redirect('/dashboard')}
-      else {
-        res.redirect('/adminpage')
-      };
-    }).catch(error => {
+    req.session.isAdmin = done.isAdmin;
+    req.session.userUuid = done.userUuid;
+    res.redirect('/dashboard');
+  }).catch(error => {
     res.json(error);
-    });
+  });
 });
-
 
 router.get('/logout', function(req, res, next) {
   req.session.isAuthenticated = false;
   res.redirect('/login');
 });
 
-
 router.get('/dashboard', function(req, res, next) {
   if (!req.session.isAuthenticated) {
     res.redirect('/login');
     return;
   }
-  res.render('dashboard', { title: 'Dashboard' });
-});
 
-
-router.get('/adminpage', function(req, res, next) {
-  if (!req.session.isAuthenticated) {
-    res.redirect('/login');
+  if (req.session.isAdmin) {
+    res.render('dashboard', {
+      title: 'Admin dashboard',
+      isAdmin: req.session.isAdmin
+    });
     return;
   }
-  res.render('adminpage', { title: 'Admin Page' });
+
+  services.getUserUploads(req).then(done => {
+    res.render('dashboard', {
+      title: 'Dashboard',
+      isAdmin: req.session.isAdmin,
+      files: done.files
+    });
+  }).catch(error => {
+    res.render('dashboard', {
+      title: 'Dashboard',
+      isAdmin: req.session.isAdmin,
+      files: []
+    });
+  });
 });
 
-
-router.get('/upload', function(req, res, next) {
+router.post('/har_upload', upload.single('har_upload'), function (req, res, next) {
   if (!req.session.isAuthenticated) {
-    res.redirect('/login');
+    res.json({
+      status: 'error',
+      message: 'Unauthorized user'
+    });
     return;
   }
-  res.render('upload', { title: 'Upload HAR File' });
+
+  services.handleUpload(req).then(done => {
+    res.redirect('/dashboard');
+  }).catch(error => {
+    res.redirect('/dashboard');
+  });
 });
 
- 
+router.post('/har_download', function (req, res, next) {
+  if (!req.session.isAuthenticated) {
+    res.json({
+      status: 'error',
+      message: 'Unauthenticated request'
+    })
+    return;
+  }
+
+  services.handleDownloadHar(req).then(done => {
+    res.download(done.path, done.filename);
+  }).catch(error => {
+    res.json(error);
+  });
+});
+
+
+
+
+router.post('/update_username', function (req, res, next) {
+  services.updateUsername(req.body).then(done => {
+    res.redirect('/dashboard');
+  }).catch(error => {
+    res.redirect('/dashboard');
+  });
+});
+
+router.post('/update_password', function (req, res, next) {
+  services.updatePassword(req.body).then(done => {
+    res.redirect('/dashboard');
+  }).catch(error => {
+    res.redirect('/dashboard');
+  });
+});
+
+
 module.exports = router;
